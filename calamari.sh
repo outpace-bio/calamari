5f3b6096e83d07aa44f7710656e5d3723424e4e5
ailurophilia <davidseversonclausen@gmail.com>
2023-10-19 15:19:52 -0700

    Adjust csv_to_fasta.R so that csv file can have any name (so long as it has csv suffix). Feed appropriate arguments to csv_to_fasta so it has access to fastas directory.

--- calamari.sh
+++ calamari.sh
@@ -14,10 +14,15 @@ echo "Fasta directory set to ${FASTAS_DIR}."
 echo "Docker image specified as ${DOCKER_IMAGE}."
 echo "Miseq output directory set to ${MISEQ_OUTPUTS}."
 
+#download fasta creation script, move it, and run it
+curl -o csv_to_fasta.R https://raw.githubusercontent.com/outpace-bio/calamari/main/csv_to_fasta.R
+mv csv_to_fasta.R $FASTAS_DIR
+Rscript $(pwd) "${FASTAS_DIR}/csv_to_fasta.R"
 
+# download fastq processing script, move it, and run it
 curl -o rename_fastqs.R https://raw.githubusercontent.com/outpace-bio/calamari/main/rename_fastqs.R
 mv rename_fastqs.R $MISEQ_OUTPUTS
-
 Rscript "${MISEQ_OUTPUTS}/rename_fastqs.R" $(pwd) $MISEQ_OUTPUTS
 
+#run Octant's pipeline script
 ./run-octopus-analysis.sh -a $FASTAS_DIR -d $DOCKER_IMAGE -q $MISEQ_OUTPUTS
--- csv_to_fasta.R
+++ csv_to_fasta.R
@@ -3,11 +3,35 @@ if(!("seqinr" %in% installed.packages())){
   install.packages("seqinr")
 }
 library(seqinr)
+args = commandArgs(trailingOnly=TRUE)
 
-plasmids <- read.csv("fastas.csv")
+message("Starting working directory is ",args[1])
+message("Fastas output directory is ",args[2])
+
+setwd(args[2])
+
+message(paste("Converting csv file in ",getwd()," to fasta file.",sep = ""))
+filenames <- list.files()
+if(length(filenames)==0){
+  stop("You must include a csv file containing reference sequences in your fastas directory.")
+}
+which_csv <- sapply(filenames,function(x) grepl(".csv",x,fixed = TRUE))
+if(sum(which_csv) ==0){
+  stop("No csv file detected in fastas directory.")
+}
+
+if(sum(which_csv)>1){
+  stop("More than one csv file detected in fastas directory.")
+}
+
+fasta_csv_file <- filenames[which_csv]
+
+plasmids <- read.csv(fasta_csv_file)
 plasmids$Sequence <- sapply(plasmids$Sequence,
                             tolower)
 
 write.fasta(sequences = lapply(1:nrow(plasmids), function(i) plasmids$Sequence[i]),
             names = plasmids$Registry.ID,
             file.out = "fastas.fasta")
+
+setwd(args[1])
